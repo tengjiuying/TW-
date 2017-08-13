da863f067677ff69262cbf9ac96d8ab14ebeb9bd
TengFeng2017 <2940681539@qq.com>
2017-08-13 21:11:47 +0800

    8.13日挑战.提交

---
+++ task-5.js
@@ -0,0 +1,13 @@
+function find(collection) {
+    let median = 0;
+    collection = collection.sort((fir, las)=>{return fir - las});
+    if ((collection.length % 2) === 0){
+        median = (collection[collection.length/2-1] + collection[collection.length/2])/2;
+    }else {
+        median = collection[parseInt(collection.length/2)];
+    }
+    return median;
+}
+
+console.log(find([2,9,4,7]));
+console.log(find([1,8,3,5,2]));
